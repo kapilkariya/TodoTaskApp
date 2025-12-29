ecfaf11b2f7f564dd7bd3f1057d26a7b24952715
kapilkariya <kapilkariya1105@gmail.com>
2025-12-29 16:39:26 +0530

    final working prod

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -2,14 +2,12 @@ const mongoose= require("mongoose");
 
 const conn=async(req,res)=>{
   try{
-    await mongoose.connect("mongodb+srv://kapilkariya77_db_user:kapilkariya77@cluster0.jpburzo.mongodb.net/").then(()=>{
+    await mongoose.connect("mongodb+srv://kapilkariya77_db_user:kapilkariya77@cluster0.4nrb8fv.mongodb.net/").then(()=>{
     console.log("connected")
   })
   }
   catch(error){
-    rex.status(400).json({
-      message:"not connected",
-    })
+   console.log(object)
   }
 }
 conn()
